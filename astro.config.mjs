85ca6fa10c8de61e0efcb7ed99a77902c36de16d
Wing <114337707+wing-0902@users.noreply.github.com>
2025-11-15 10:17:31 +0000

    add vuejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte(), vue()]
 });
