bc3e847bc176602d538894309ce8415b08a4854b
Wing <114337707+wing-0902@users.noreply.github.com>
2025-11-19 06:27:51 +0000

    pagefindStyle

--- src/components/SearchUI/PagefindConnector.svelte
+++ src/components/SearchUI/PagefindConnector.svelte
@@ -19,25 +19,3 @@
 <section class="searchBox">
   <div id="search" class='pgSearch'></div>
 </section>
-
-<style lang="scss">
-  .searchBox {
-    margin-top: 95px;
-    .pgSearch {
-      --pagefind-ui-primary: var(--a-default);
-      --pagefind-ui-primary-hover: var(--a-hover);
-      --pagefind-ui-scale: 1;
-      --pagefind-ui-tag-text: var(--foreground-sub);
-      --pagefind-ui-text: var(--foreground);
-      --pagefind-ui-background: transparent;
-      --pagefind-ui-border: var(--foreground);
-      --pagefind-ui-tag: var(--foreground-sub);
-      --pagefind-ui-border-width: 2px;
-      --pagefind-ui-border-radius: 31px;
-      --pagefind-ui-image-border-radius: 8px;
-      --pagefind-ui-image-box-ratio: 3 / 2;
-      --pagefind-ui-font: var(--font-zen-kaku-gothic-new);
-      --pagefind-ui-tag: rgba(0, 16, 72, 0.7);
-    }
-  }
-</style>
