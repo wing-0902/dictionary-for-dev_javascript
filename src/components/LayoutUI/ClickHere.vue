4454de11449abda6c7e2c196a2ab1a617f20fca1
Wing <114337707+wing-0902@users.noreply.github.com>
2025-12-08 08:37:23 +0000

    とりあえず？

---
+++ src/components/LayoutUI/ClickHere.vue
@@ -0,0 +1,25 @@
+<script lang='ts' setup>
+  import SeeMore from '../Icons/SeeMore.vue';
+</script>
+
+<script lang='ts'>
+
+</script>
+
+<template>
+  <div class='root'>
+    <button>
+      <SeeMore />
+    </button>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+  .root {
+    button {
+      border: none;
+      background: transparent;
+      color: var(--foreground);
+    }
+  }
+</style>
