85a376341ec65c373abc7c2d85f1d1d389ce483b
Wing <114337707+wing-0902@users.noreply.github.com>
2025-12-18 23:37:43 +0000

    alpinejs

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,6 +1,8 @@
 import type { Alpine } from 'alpinejs';
 import persist from '@alpinejs/persist';
+import mask from '@alpinejs/mask';
 
 export default (Alpine: Alpine) => {
   Alpine.plugin(persist);
+  Alpine.plugin(mask);
 }
