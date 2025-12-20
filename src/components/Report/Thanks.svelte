e7b409ee3460cd6820366dffd30f82e82c8c22d4
Wing <114337707+wing-0902@users.noreply.github.com>
2025-12-20 06:56:36 +0000

    CSSを省略

--- src/components/Report/Thanks.svelte
+++ src/components/Report/Thanks.svelte
@@ -2,6 +2,8 @@
   import { onMount } from 'svelte';
   import { reportToDel } from '../Form/formSessionStorage.mts';
 
+  import Required from '$components/Form/Required.svelte';
+
   let username: string;
   let email: string;
   let reportTitle: string;
@@ -29,23 +31,74 @@
   })
 </script>
 
-<ul>
-  <li>
-    <strong>お名前</strong><span>：{username}</span>
-  </li>
-  <li>
-    <strong>メール</strong><span>：{email}</span>
-  </li>
-
-</ul>
+<div class='root'>
+  <section>
+    <fieldset class='aboutYou'>
+      <legend>あなたについて</legend>
+      <div class='spacer'>
+        <Required />
+        <label for='name'>お名前</label><br/>
+        <input readonly name='name' type='text' value={username} class='formTextSlot' />
+      </div>
+      <br />
+      <div class='spacer'>
+        <Required />
+        <label for='email'>メールアドレス</label><br/>
+        <input readonly name='email' type='email' value={email} class='formTextSlot' />
+      </div>
+    </fieldset>
+    <fieldset class='本題'>
+      <legend>レポート</legend>
+      <div class='spacer'>
+        <Required />
+        <label for='report_title'>タイトル</label><br/>
+        <input readonly name='report_title' type='text' value={reportTitle} class='formTextSlot' />
+      </div>
+      <div>
+        <Required />
+        <label for='report_message'>本文</label><br/>
+        <textarea readonly name='report_message' placeholder='ここに本文を入力...' value={reportMsg} class='formTextSlot'></textarea>
+      </div>
+    </fieldset>
+  </section>
+</div>
 
 <style lang="scss">
-  ul {
-    font-family: var(--font-zen-maru-gothic);
-    li {
-      overflow-x: wrap;
-      strong {
-        font-family: var(--font-zen-kaku-gothic-new);
+  .root {
+    font-family: var(--font-zen-kaku-gothic-new);
+    box-sizing: border-box;
+    section {
+      // formブロックのスタイル
+      margin: auto 10px;
+      box-sizing: border-box;
+
+      .aboutYou {
+        max-width: 100%;
+        .spacer {
+          align-items: last baseline;
+          overflow-x: wrap;
+          input {
+            width: 100%;
+            box-sizing: border-box;
+            min-width: 280px;
+            margin: 0;
+            height: 40px;
+            &[type='email'] {
+              font-family: var(--font-fira-code);
+            }
+          }
+        }
+      }
+      .本題 {
+        max-width: 100%;
+        textarea {
+          width: 100%;
+          box-sizing: border-box;
+          font-size: 19px;
+          height: 100px;
+          resize: none;
+          background: var(--codeBack);
+        }
       }
     }
   }
