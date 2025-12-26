72f84aab729d78c3dcdd9362ea37b6bcc27682e7
tsubasa.terasawa0902 <tsubasa.terasawa0902@gmail.com>
2025-12-26 11:03:07 +0000

    こういうこと？

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,12 +6,8 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
-    pkgs.corepack_21
+    pkgs.nodejs_20
+    pkgs.nodePackages.pnpm
   ];
 
   # Sets environment variables in the workspace
@@ -19,23 +15,17 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "ms-vscode.vscode-typescript-next"
     ];
 
     # Enable previews
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          command = ["pnpm" "run" "dev" "--host" "0.0.0.0" "--port" "$PORT"];
+          manager = "web";
+        };
       };
     };
 
@@ -45,6 +35,7 @@
       onCreate = {
         # Example: install JS dependencies from NPM
         # npm-install = "npm install";
+        pnpm-install = "pnpm install";
       };
       # Runs when the workspace is (re)started
       onStart = {
