03c9819b23b8a7ce47e8cbc73bf7000214a9c17b
tsubasa.terasawa0902 <tsubasa.terasawa0902@gmail.com>
2025-12-26 23:42:58 +0000

    なるほどね

--- src/components/PagefindNew/ResultSlot.svelte
+++ src/components/PagefindNew/ResultSlot.svelte
@@ -4,11 +4,31 @@
 </script>
 
 <section class='root'>
-
+  {#if searchWord === ''}
+    <div class='message'>
+      {#if (searchMode === '見出し')}
+      {:else if (searchMode === '用例')}
+        <p>このモードでは，コードブロック内の用例を検索します．</p>
+      {:else if (searchMode === '全文')}
+        <p>このモードでは，コンテンツの全文を検索できます．</p>
+      {:else}
+        <p>検索モードを選択してください．</p>
+      {/if}
+    </div>
+  {:else}
+  {/if}
 </section>
 
 <style lang="scss">
   .root {
     height: calc(100dvh - var(--headerHeight) - 68px);
+    .message {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      p {
+        text-align: center;
+      }
+    }
   }
 </style>
