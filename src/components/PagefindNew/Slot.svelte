a1806622cefabfa33e22f3576876825f491a42a5
tsubasa.terasawa0902 <tsubasa.terasawa0902@gmail.com>
2025-12-26 13:15:43 +0000

    上の検索スロット

--- src/components/PagefindNew/Slot.svelte
+++ src/components/PagefindNew/Slot.svelte
@@ -3,7 +3,7 @@
   export let searchWord: string;
 
   let hydrated = false;
-  
+
   onMount(() => {
     searchWord = sessionStorage.getItem('searchWord') ?? '';
     hydrated = true;
@@ -22,6 +22,15 @@
       placeholder='入力して検索'
       aria-label='検索語句を入力'
       bind:value={searchWord}/>
+    {#if (searchWord !== '')}
+      <button
+        aria-label='検索クエリを削除'
+        on:click={() => searchWord = ''}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+          <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+        </svg>
+      </button>
+    {/if}
   </div>
 </div>
 
