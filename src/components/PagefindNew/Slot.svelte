19a726d4de9d64b8965d0c422da5d0f9ad3ee08a
tsubasa.terasawa0902 <tsubasa.terasawa0902@gmail.com>
2025-12-26 13:49:44 +0000

    素晴らしい

--- src/components/PagefindNew/Slot.svelte
+++ src/components/PagefindNew/Slot.svelte
@@ -28,13 +28,13 @@
         aria-label='検索クエリを削除'
         on:click={() => searchWord = ''}>
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
-          <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+          <path d="M480-424 284-228q-11 11-28 11t-28-11q-11-11-11-28t11-28l196-196-196-196q-11-11-11-28t11-28q11-11 28-11t28 11l196 196 196-196q11-11 28-11t28 11q11 11 11 28t-11 28L536-480l196 196q11 11 11 28t-11 28q-11 11-28 11t-28-11L480-424Z"/>
         </svg>
       </button>
     {/if}
   </div>
   <div class='historySlot'>
-    <button>
+    <button class='history'>
       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path d="M480-120q-126 0-223-76.5T131-392q-4-15 6-27.5t27-14.5q16-2 29 6t18 24q24 90 99 147t170 57q117 0 198.5-81.5T760-480q0-117-81.5-198.5T480-760q-69 0-129 32t-101 88h70q17 0 28.5 11.5T360-600q0 17-11.5 28.5T320-560H160q-17 0-28.5-11.5T120-600v-160q0-17 11.5-28.5T160-800q17 0 28.5 11.5T200-760v54q51-64 124.5-99T480-840q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-480q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-120Zm40-376 100 100q11 11 11 28t-11 28q-11 11-28 11t-28-11L452-452q-6-6-9-13.5t-3-15.5v-159q0-17 11.5-28.5T480-680q17 0 28.5 11.5T520-640v144Z"/>
       </svg>
@@ -44,13 +44,14 @@
 
 <style lang='scss'>
   .root {
+    $historyWidth: 36px;
+    $side-margin: 7px;
     width: 100%;
     display: flex;
     .inputSlot {
-      width: calc(100% - 36px);
       position: relative;
-      $side-margin: 7px;
-      margin: 0 $side-margin;
+      width: calc(100% - ($historyWidth + $side-margin));
+      margin-left: $side-margin;
       input {
         background: var(--codeBack);
         color: var(--foreground);
@@ -75,20 +76,25 @@
         top: 0;
         right: 0;
         border: none;
-        height: 40px;
+        height: 100%;
+        display: flex;
+        align-items: center;
         background: transparent;
         color: var(--foreground);
       }
     }
     .historySlot {
-      width: 36px;
-      button {
+      width: $historyWidth;
+      margin-right: $side-margin;
+      button.history {
+        background: transparent;
         width: 100%;
         border: none;
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100%;
+        color: var(--foreground);
       }
     }
   }
