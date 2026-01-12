27ef5abc6dc0ea708784c47d3d0d16814d2d9ebc
Wing <114337707+wing-0902@users.noreply.github.com>
2026-01-12 10:03:01 +0000

    scale

--- src/components/PagefindNew/History.svelte
+++ src/components/PagefindNew/History.svelte
@@ -60,14 +60,20 @@
     class="slot"
     class:isClosed={!isOpen}
   >
-    <h3>検索履歴</h3>
+    <div class="header">
+      <div class="side"></div>
+      <h3>検索履歴</h3>
+      <div class="side">
+        <button aria-label='履歴を全消去'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M280-120q-33 0-56.5-23.5T200-200v-520q-17 0-28.5-11.5T160-760q0-17 11.5-28.5T200-800h160q0-17 11.5-28.5T400-840h160q17 0 28.5 11.5T600-800h160q17 0 28.5 11.5T800-760q0 17-11.5 28.5T760-720v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM400-280q17 0 28.5-11.5T440-320v-280q0-17-11.5-28.5T400-640q-17 0-28.5 11.5T360-600v280q0 17 11.5 28.5T400-280Zm160 0q17 0 28.5-11.5T600-320v-280q0-17-11.5-28.5T560-640q-17 0-28.5 11.5T520-600v280q0 17 11.5 28.5T560-280ZM280-720v520-520Z"/></svg></button>
+      </div>
+    </div>
     <ul>
       {#each historyList as word}
         <li>
           <button
             aria-label={`${word}を検索`}
             onclick={() => handleClick(word)}
-            class='searchOne'
+            class="searchOne"
           >
             {word}
           </button>
@@ -143,10 +149,30 @@
       display: flex;
       flex-direction: column;
       // slotの中身
-      h3 {
-        text-align: center;
-        margin: 5px 0 0 0;
+      .header {
+        display: flex;
+        padding: 0 7px;
+        justify-content: space-between;
+        align-items: center;
+        transform: translateY(6px);
         height: 25px;
+        .side {
+          width: 25px;
+          height: 100%;
+          button {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+            border: none;
+            background: transparent;
+            color: var(--foreground);
+            svg {
+              transform: scale(160%);
+            }
+          }
+        }
       }
       ul {
         height: calc(100% - 25px);
